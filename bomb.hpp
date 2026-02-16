0ce43e1f7c3c62e13590c2687654fc976d933d61
Raiwin <raiwin.ko@gmail.com>
2026-02-16 15:45:25 +0700

    เเก้ละเบิด15วิ

--- bomb.hpp
+++ bomb.hpp
@@ -13,7 +13,7 @@ private:
     bool active;
     sf::Clock spawnClock;
 
-    const float SPAWN_TIME = 180.f; // 3 นาที
+    const float SPAWN_TIME = 15.f; // เปลี่ยนเป็น 15 วินาที ระเบิดจะตกลงมาเร็วขึ้น 
 
 public:
     Bomb()
