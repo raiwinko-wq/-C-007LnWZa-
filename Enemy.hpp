cb9b00dad69e24ddd1e5304a5ca1975806d3adf4
Rada <sirada_dee@cmu.ac.th>
2026-02-23 16:39:20 +0700

    edit type3 enemy from moving upward

--- Enemy.hpp
+++ Enemy.hpp
@@ -9,7 +9,8 @@ public:
     float speed;
     int type, hp, flashTimer = 0, freezeTimer = 0;
 
-    void init(sf::Texture& tex, int enemyType, float startX, float baseSpeed) {
+    void init(sf::Texture& tex, int enemyType, float startX, float baseSpeed)
+    {
         type = enemyType;
         sprite.setTexture(tex);
         // [แก้ไข] ปรับขนาดศัตรูให้เหมาะสม
@@ -17,7 +18,7 @@ public:
         sprite.setOrigin(sprite.getLocalBounds().width / 2.0f, sprite.getLocalBounds().height / 2.0f);
         sprite.setPosition(startX, -60);
 
-        if (type == 1) { speed = baseSpeed; hp = 9999; }
+        if (type == 1){ speed = baseSpeed; hp = 9999; }
         else if (type == 3) { speed = baseSpeed * 0.6f; hp = 2; }
         else { speed = baseSpeed - 0.5f; hp = 1; }
     }
@@ -30,14 +31,32 @@ public:
         return sf::FloatRect(b.left + (b.width - w)/2, b.top + (b.height - h)/2, w, h);
     }
 
-    void update(sf::Vector2f playerPos) {
+    void update(sf::Vector2f playerPos) 
+    {
         if (freezeTimer > 0) { freezeTimer--; return; }
-        if (type == 3) {
-            float dx = playerPos.x - sprite.getPosition().x, dy = playerPos.y - sprite.getPosition().y;
+
+        void update(sf::Vector2f playerPos);
+        {
+            if (freezeTimer > 0) { freezeTimer--; return; }
+
+            if (type == 3) {
+             float dx = playerPos.x - sprite.getPosition().x;
+             float dy = playerPos.y - sprite.getPosition().y;
+
+             // ❗ ห้ามเดินขึ้น
+            if (dy < 0) dy = speed;
+
             float dist = std::sqrt(dx * dx + dy * dy);
-            if (dist != 0) sprite.move((dx/dist)*speed, (dy/dist)*speed);
-        } else sprite.move(0, speed);
-        if (flashTimer > 0) flashTimer--;
+            if (dist != 0)
+            sprite.move((dx/dist)*speed, (dy/dist)*speed);
+            }
+            else 
+            {
+                sprite.move(0, speed);
+            }
+
+             if (flashTimer > 0) flashTimer--;
+        }
     }
 
     void draw(sf::RenderWindow& window) {
